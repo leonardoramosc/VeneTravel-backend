16aaa02762a37b0fe0f7fb6f39719af72a548aee
Leonardo Ramos <leorrc96@gmail.com>
2021-09-03 11:30:06 -0400

    update(app.js): global error handler middleware added

--- app.js
+++ app.js
@@ -15,4 +15,25 @@ app.use(express.json());
 // Routes
 app.use('/api/v1/tours', tourRouter);
 
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // If next receives an argument, express will interpret it as
+  // an error.
+  next(err);
+});
+
+// Error handler middleware: express only call this when an error is detected.
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
