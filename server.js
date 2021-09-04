41b4e2c02f16262b1cc02a22b323de7783ea642a
Leonardo Ramos <leorrc96@gmail.com>
2021-09-04 18:02:55 -0400

    update(server): handle uncaught exceptions

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION. Shutting down...');
+  console.log(err);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const { DB_LOCAL_URI } = process.env;
@@ -23,9 +29,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION. Shutting down...');
-
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
