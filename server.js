2a09e5b393552ad9cb35b9fbce4b2ead6a35a7f5
Leonardo Ramos <leorrc96@gmail.com>
2021-09-04 17:49:38 -0400

    update(server): close server connection after unhandledRejection

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ mongoose
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log(`app running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION. Shutting down...');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
