65c4921238632bddbe95f2ae3807787d06c00265
Leonardo Ramos <leorrc96@gmail.com>
2021-09-08 22:25:41 -0400

    update(authController): send email to reset password on forgot

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -118,12 +119,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // we need to save this change in the database, but also we need to disable the validators in order
   // to avoid errors for example with required fields.
   await user.save({ validateBeforeSave: false });
-  // send it to user's email
 
-  res.status(200).json({
-    status: 'success',
-    resetToken,
-  });
+  // send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new 
+  password and passwordConfirm to: ${resetURL}.\nIf you did not forget your password, please ignore this message.`;
+
+  try {
+    await sendEmail({
+      to: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      text: message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpired = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later', 500)
+    );
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
---
+++ utils/email.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async ({ to, subject, text }) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Leonardo Ramos <venetravel2021@gmail.com>',
+    to,
+    subject,
+    text,
+  };
+
+  // 3) Send email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
