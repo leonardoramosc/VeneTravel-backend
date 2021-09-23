71e24aa36c488969446839268eb1cad97c1dea35
Leonardo Ramos <leorrc96@gmail.com>
2021-09-23 14:19:18 -0400

    update(tour): create route to get distances from one point to the tours

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -120,3 +120,50 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// get distances between given coordinates and all tours.
+exports.getDistances = catchAsync(async (req, res, next) => {
+  // /tours-within/:distance/center/:latlng/unit/:unit
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const milesPerMeter = 0.000621371;
+  const kilometersPerMeter = 0.001;
+
+  const multiplier = unit === 'miles' ? milesPerMeter : kilometersPerMeter;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format: lat,lng',
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        // since the distance is in meters we need to convert it by the unit (kilometers or miles)
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      distances,
+    },
+  });
+});
