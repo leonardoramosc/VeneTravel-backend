4412988d3373e30148cbd2a91f39ac4b8c93580e
Leonardo Ramos <leorrc96@gmail.com>
2021-09-23 10:57:43 -0400

    update(tour.model): add index to the startLocation field

--- models/tour.model.js
+++ models/tour.model.js
@@ -118,6 +118,7 @@ const tourSchema = new mongoose.Schema(
 // Agregar index en price en orden ascendente (-1 siquieremos que sea descendente)
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' }); // for geoespacial data.
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
