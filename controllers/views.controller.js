e4f96bc94b87c6f28492b011d12abd66044f20cd
Leonardo Ramos <leorrc96@gmail.com>
2021-09-26 21:48:42 -0400

    update(getTour): populate reviews on tour

--- controllers/views.controller.js
+++ controllers/views.controller.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tour.model');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
@@ -10,8 +11,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
