403786a0b6ecaac58f6202140e322412997e91f7
Leonardo Ramos <leorrc96@gmail.com>
2021-09-28 20:40:29 -0400

    feat(map): display tours locations on map using mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibGVvcnJjIiwiYSI6ImNrdTJyYW95ajE0OTEydXFyemY5OXlhcnIifQ.LiuRx4U-qGo6QavObqfo1w';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/leorrc/cku2u9zvm013f17mcy9i76rme',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // add marker
+  const markerEl = document.createElement('div');
+  markerEl.className = 'marker';
+
+  new mapboxgl.Marker(markerEl).setLngLat(loc.coordinates).addTo(map);
+
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
