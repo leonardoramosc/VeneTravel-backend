7933692796e2d3bdd916bcfea5d14066947f0112
Leonardo Ramos <leorrc96@gmail.com>
2021-10-03 14:49:28 -0400

    update(resizeUserPhoto): await image processing before calling next

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -29,19 +29,19 @@ const upload = multer({
   fileFilter: multerFilter,
 });
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 exports.uploadUserPhoto = upload.single('photo');
 
